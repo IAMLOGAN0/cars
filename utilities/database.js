4841acf8b30b4abf1518d363fef240792f658228
Soumendu <soumendugiri77492652@gmail.com>
2025-08-12 00:44:14 +0530

    fix

--- utilities/database.js
+++ utilities/database.js
@@ -9,29 +9,23 @@ function getTimezoneOffset(timezone) {
 }
 
 // Initialize Sequelize Object
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    dialect: "mysql",
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    pool: {
-      max: 512,
-      min: 0,
-      acquire: 60000,
-      idle: 30000,
-    },
-    logging: process.env.SQL_LOG === "1" ? console.log : false,
-    timezone: getTimezoneOffset(process.env.APP_TIMEZONE),
-    retry: {
-      match: [/Deadlock/i, Sequelize.ConnectionError], // Retry on connection errors
-      max: 3, // Maximum retry 3 times
-      backoffBase: 3000, // Initial backoff duration in ms. Default: 100,
-      backoffExponent: 1.5, // Exponent to increase backoff each try. Default: 1.1
-    },
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "mysql",
+  pool: {
+    max: 512,
+    min: 0,
+    acquire: 60000,
+    idle: 30000,
+  },
+  logging: process.env.SQL_LOG === "1" ? console.log : false,
+  timezone: getTimezoneOffset(process.env.APP_TIMEZONE),
+  retry: {
+    match: [/Deadlock/i, Sequelize.ConnectionError],
+    max: 3,
+    backoffBase: 3000,
+    backoffExponent: 1.5,
+  },
+});
+
 
 module.exports = sequelize;
